49fbbc0a99524cada470f4b39ce8bac705920880
aliharslan0 <97433474+aliharslan0@users.noreply.github.com>
2022-07-16 10:54:44 +0300

    Update main.py
    - Lexbor parser
    - PriorityQueue
    - Background downloading

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from atexit import register
 from concurrent.futures import ThreadPoolExecutor
 from pathlib import Path
-from queue import Queue
+from queue import PriorityQueue
 from shutil import rmtree
 from subprocess import Popen, PIPE
 from tempfile import TemporaryDirectory
 from time import perf_counter
 
 from requests import Session
-from selectolax.parser import HTMLParser
+from selectolax.lexbor import LexborHTMLParser
 
 temp_dir = TemporaryDirectory()
 temp_folder = Path(temp_dir.name)
@@ -17,7 +17,7 @@ session.headers['User-Agent'] = 'Mozilla'
 
 
 class Downloader:
-    _QUEUE = Queue()
+    _QUEUE = PriorityQueue()
     _QUEUE_LENGTH = 0
 
     @classmethod
@@ -26,18 +26,18 @@ class Downloader:
         start = perf_counter()
         with temp_folder.joinpath(file_name).open('wb') as dl_file:
             dl_file.write(session.get(url).content)
-        cls._QUEUE.put((file_name, perf_counter() - start))
+        cls._QUEUE.put((perf_counter() - start, file_name))
 
     @classmethod
-    def apkmirror(cls, version: str, music: bool = False) -> None:
+    def apkmirror(cls, version: str, music: bool) -> None:
         app = 'youtube-music' if music else 'youtube'
         version = '-'.join(v.zfill(2 if i else 0) for i, v in enumerate(version.split('.')))
 
         page = 'https://www.apkmirror.com/apk/google-inc/{a}/{a}-{v}-release/{a}-{v}-android-apk-download/'
-        parser = HTMLParser(session.get(page.format(v=version, a=app)).content)
+        parser = LexborHTMLParser(session.get(page.format(v=version, a=app)).text)
 
         resp = session.get('https://www.apkmirror.com' + parser.css_first('a.accent_bg').attributes['href'])
-        parser = HTMLParser(resp.content)
+        parser = LexborHTMLParser(resp.text)
 
         href = parser.css_first('p.notes:nth-child(3) > span:nth-child(1) > a:nth-child(1)').attributes['href']
         cls._download('https://www.apkmirror.com' + href, 'youtube.apk')
@@ -45,16 +45,16 @@ class Downloader:
     @classmethod
     def repository(cls, name: str) -> None:
         resp = session.get(f'https://github.com/revanced/revanced-{name}/releases/latest')
-        parser = HTMLParser(resp.content)
+        parser = LexborHTMLParser(resp.text)
         url = parser.css('li.Box-row > div:nth-child(1) > a:nth-child(2)')[:-2][-1].attributes['href']
         cls._download('https://github.com' + url, Path(url).with_stem(name).name)
 
     @classmethod
-    def report(cls):
+    def report(cls) -> None:
         started = False
         while True:
             item = cls._QUEUE.get()
-            print(f'{item[0]} downloaded in {item[1]:.2f} seconds.')
+            print(f'{item[1]} downloaded in {item[0]:.2f} seconds.')
             cls._QUEUE.task_done()
             cls._QUEUE_LENGTH -= 1
 
@@ -81,16 +81,10 @@ class Patches:
         self._yt = youtube
         self._ytm = music
 
-    @property
-    def youtube(self) -> list[dict[str, str]]:
-        return self._yt
-
-    @property
-    def music(self) -> list[dict[str, str]]:
-        return self._ytm
-
-    def version(self, music: bool = False) -> str:
-        return next(i['version'] for i in (self._ytm if music else self._yt) if i['version'] != 'all')
+    def get(self, music: bool):
+        patches = self._ytm if music else self._yt
+        version = next(i['version'] for i in patches if i['version'] != 'all')
+        return patches, version
 
 
 class ArgParser:
@@ -135,34 +129,34 @@ def main():
     downloader = Downloader
     arg_parser = ArgParser
 
-    selected_app = input('Youtube or Youtube Music? [YT/YTM]: ').lower().strip()
-    if selected_app not in ('yt', 'ytm'):
-        raise Exception(f'{selected_app} is not valid choice.')
-    music = True if selected_app == 'ytm' else False
-    selected_app = patches.youtube if not music else patches.music
+    def get_patches():
+        for i, v in enumerate(app_patches):
+            print(f'[{i:>02}] {v["name"]:<32}: {v["description"]}')
 
-    for i, v in enumerate(selected_app):
-        print(f'[{i:>02}] {v["name"]:<32}: {v["description"]}')
+        selected_patches = input('Select the patches you want as "0 2 1 ...": ').split(' ')
+        selected_patches = list(set(map(int, [i.strip() for i in selected_patches if i.strip() and i.isdigit()])))
 
-    selected_patches = input('Select the patches you want as "0 2 1 ...": ').split(' ')
-    selected_patches = list(set(map(int, [i.strip() for i in selected_patches if i.strip() and i.isdigit()])))
+        if any(not 0 <= x < len(app_patches) for x in selected_patches):
+            raise Exception('Some of the selected patches are not valid.')
 
-    if any(x >= len(selected_app) or x < 0 for x in selected_patches):
-        raise Exception('Some of the selected patches are not valid.')
+        selected_patches = [v['name'] for i, v in enumerate(app_patches) if i not in selected_patches]
+        for sp in selected_patches:
+            arg_parser.exclude(sp)
 
-    selected_patches = [v['name'] for i, v in enumerate(selected_app) if i not in selected_patches]
-    for sp in selected_patches:
-        arg_parser.exclude(sp)
+    app = input('Youtube or Youtube Music? [YT/YTM]: ').lower().strip()
+    if app not in ('yt', 'ytm'):
+        raise Exception(f'{app} is not valid choice.')
+    music = app == 'ytm'
+    app_patches, version = patches.get(music)
 
-    print('Downloading necessary files...')
     with ThreadPoolExecutor() as executor:
         executor.map(downloader.repository, ('cli', 'integrations', 'patches'))
-        executor.submit(downloader.apkmirror, patches.version(music), music)
-        executor.submit(downloader.report)
+        executor.submit(downloader.apkmirror, version, music)
+        executor.submit(get_patches).add_done_callback(lambda _: downloader.report())
     print('Download completed.')
 
     arg_parser.run()
-    print('Wait for the programme to exit.')
+    print('Wait for programme to exit.')
 
 
 if __name__ == '__main__':
